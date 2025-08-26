d20c0828f25f2055614ea4554a338c8e86b02f24
Kaylin <215088973+Kaylin-Chu@users.noreply.github.com>
2025-08-27 11:58:50 +1200

    deleted  shoppingList migration to fix render issue

--- server/db/migrations/store.js
+++ server/db/migrations/store.js
@@ -22,3 +22,26 @@ export async function up(knex) {
 export function down(knex) {
   return knex.schema.dropTable('store');
 };
+
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+// export async function up(knex) {
+//   await knex.schema.createTable('shopping_list', (table) => {
+//     table.string('item_name').references('store.item_name')
+//     //.notNullable();
+//     table.integer('quantity')
+//     //.notNullable().defaultTo(1);
+//     table.boolean('purchased')
+//     //.notNullable().defaultTo(false);
+//   })
+// }
+
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+// export function down(knex) {
+//   return knex.schema.dropTable('shopping_list')
+// }
