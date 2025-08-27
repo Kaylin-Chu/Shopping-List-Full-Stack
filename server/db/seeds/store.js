fccf3d7c6082ebe1231fc351f6457b10f7a9962a
Kaylin <215088973+Kaylin-Chu@users.noreply.github.com>
2025-08-27 12:04:08 +1200

    deleted  shoppingList seed to fix render issue

--- server/db/seeds/store.js
+++ server/db/seeds/store.js
@@ -11,3 +11,17 @@ export async function seed(knex) {
     {id: 3, item_name: 'Milk', price: 1.00, stock_quantity: 75}
   ]);
 };
+
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> } 
+//  */
+// export async function seed(knex) {
+//   // Deletes ALL existing entries
+//   await knex('shopping_list').del()
+//   await knex('shopping_list').insert([
+//     {item_name: 'Apples', quantity: 5, purchased: false},
+//     {item_name: 'Milk',  quantity: 2, purchased: true},
+//     {item_name: 'Bread',  quantity: 1, purchased: false}
+//   ]);
+// };
